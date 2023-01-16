064c106577d7e8ff00fafab00da79f20f09bc916
wchistow <wchistow@yandex.ru>
2023-01-16 10:56:25 +0300

    add new constructors

--- src/main.rs
+++ src/main.rs
@@ -21,46 +21,42 @@ struct Value {
 }
 
 impl Value {
-    /// Создает объект [Value] со стандартными значениями, в зависимости от типа.
-    /// Стандартные значения для разных типов:
-    ///   INT   = 5
-    ///   FLOAT = 6.2
-    ///   CHAR  = 'H'
-    fn new(value_type: ValueType) -> Self {
-        match value_type {
-        ValueType::INT => Value {
+    fn new_int(value: i32) -> Self {
+        Value {
             value_type: ValueType::INT,
-
-            int_value: Some(5),
+            int_value: Some(value),
             float_value: None,
             char_value: None
-        },
-        ValueType::FLOAT => Value {
-            value_type: ValueType::FLOAT,
-
-            int_value: None,
-            float_value: Some(6.2),
-            char_value: None
-        },
-        ValueType::CHAR => Value {
-            value_type: ValueType::CHAR,
-
-            int_value: None,
-            float_value: None,
-            char_value: Some('H')
-        },
+        }
     }
+
+    fn new_float(value: f32) -> Self {
+        Value {
+            value_type: ValueType::FLOAT,
+            int_value: None,
+            float_value: Some(value),
+            char_value: None
+        }
+    }
+
+    fn new_char(value: char) -> Self {
+        Value {
+            value_type: ValueType::CHAR,
+            int_value: None,
+            float_value: None,
+            char_value: Some(value)
+        }
     }
 }
 
 fn main() {
     // Для простоты стек заполняется фиксированными значениями.
     let stack = [
-        Value::new(ValueType::INT),
-        Value::new(ValueType::CHAR),
-        Value::new(ValueType::INT),
-        Value::new(ValueType::FLOAT),
-        Value::new(ValueType::INT),
+        Value::new_int(5),
+        Value::new_char('k'),
+        Value::new_int(123),
+        Value::new_float(10.6),
+        Value::new_int(18),
     ];
 
     // Вывод типов и значений в стеке.
